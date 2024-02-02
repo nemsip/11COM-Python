9181e274737d8231adf9ead924fab9a73f7a20c1
Cryptic1526 <nemith27dhg@gmail.com>
2024-02-02 22:43:58 +1300

    Initial commit

---
+++ GeneralPy/hello.py
@@ -0,0 +1,26 @@
+# hello.py
+
+import time
+
+time.sleep(1)
+while True:
+    try:
+        num = int(input("What number? >_ "))
+        num2 = int(input("What the other number? >_ "))
+        operation = int(input(f"What do you want to do with these two numbers?\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n>_ "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        continue
+
+    if not 1 <= operation <= 4:
+        print("Invalid input. Please choose a number between 1 and 4.")
+        continue
+
+    if operation == 1:
+        print(num + num2)
+    elif operation == 2:
+        print(num - num2)
+    elif operation == 3:
+        print(num * num2)
+    elif operation == 4:
+        print(num / num2)
