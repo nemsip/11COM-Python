e35d67cb8abc302556205ab565e4482373149b9a
Cryptic1526 <119151348+Cryptic1526@users.noreply.github.com>
2024-06-04 12:44:51 +1200

    woah

--- Flask/Project 3/profileWeb.py
+++ Flask/Project 3/profileWeb.py
@@ -12,7 +12,7 @@ def profile_form():
 
 @app.route('/profile-result')
 def profile_result():
-    fname=request.args.get('fname', "Default")
+    fname=request.args.get('fname')
     lname=request.args.get('lname')
     age=int(request.args.get('age'))
     return render_template('profile-result.html', fname=fname, lname=lname, age=age)
@@ -24,6 +24,18 @@ def method_not_allowed(error):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('/sys/404.html'), 404
+
+@app.route("/calculate")
+def calculate():
+    return render_template('calc-in.html')
+
+@app.route("/calc-out")
+def calc_out():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    oper = request.args.get('operation')
+
+    return render_template('calc-out.html', num1=num1, num2=num2, oper=oper) #, result=result)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
