3eb0e6bf1d477c43a0d0a48e9193ef48667468f8
Cryptic1526 <119151348+Cryptic1526@users.noreply.github.com>
2024-07-24 18:22:03 +1200

    added min and some minor changes

--- NCEA Practice/main.py
+++ NCEA Practice/main.py
@@ -9,18 +9,17 @@ def index():
         merit = int(request.form['merit'])
         excellence = int(request.form['excellence'])
         print(f"Achieved: {achieved}, Merit: {merit}, Excellence: {excellence}") # debugging because was not work
-        return render_template('result.html', achieved=achieved, merit=merit, excellence=excellence)
+        return render_template('result.html', achieved=achieved, merit=merit, excellence=excellence, min=min)
     return render_template('index.html')
 
-# Error Routing
-
+# ereor handling
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('sys/404.html'), 404
 
-# @app.errorhandler(500)
-# def internal_server_error(e):
-#     return render_template('sys/500.html', e=e), 500
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('sys/500.html', e=e), 500
 
 @app.errorhandler(403)
 def forbidden(e):
