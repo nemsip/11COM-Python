46f06875ed655f2658f8303ec0fcb40143df1808
Cryptic1526 <119151348+Cryptic1526@users.noreply.github.com>
2024-08-21 15:48:38 +1200

    did all the server side stuff (for now)

--- NCEA/main.py
+++ NCEA/main.py
@@ -2,10 +2,41 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        family_name = request.form.get('family-name')
+        home = request.form.get('home')
+        ocean = request.form.get('ocean')
+        mountain = request.form.get('mountain')
+        river = request.form.get('river')
+        ancestry = request.form.get('ancestry')
+        
+        ingoa = request.form.get('ingoa')
+        whanau = request.form.get('whanau')
+        marae = request.form.get('marae')
+        iwi = request.form.get('iwi')
+        maunga = request.form.get('maunga')
+        awa = request.form.get('awa')
+        waka = request.form.get('waka')
+        
+        # form.get
+        return render_template('result.html',
+                               name=name,
+                               family_name=family_name,
+                               home=home, ocean=ocean,
+                               mountain=mountain,
+                               river=river,
+                               ancestry=ancestry,
+                               
+                               ingoa=ingoa,
+                               whanau=whanau,
+                               marae=marae, iwi=iwi,
+                               maunga=maunga,
+                               awa=awa,
+                               waka=waka)
     return render_template('index.html')
-
 
 @app.errorhandler(404)
 def page_not_found(e):
